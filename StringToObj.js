2d8962cf28f75583f4ce8f15862c6288a9c7db22
cGoyal69 <127645215+cGoyal69@users.noreply.github.com>
2024-09-26 16:01:23 +0530

    string to obj

--- StringToObj.js
+++ StringToObj.js
@@ -1,11 +1,14 @@
-const originalString = '[10]';
+const originalString = '[{ $and: [{ age: { $gte: 25 } }, { salary: { $gt: 50000 } }, { department: { $in: ["Engineering", "Marketing"] } }, { status: "active" }, { hobbies: { $elemMatch: { $in: ["hiking", "gaming"] } } }] }, { name: 1, age: 1, department: 1, salary: 1, _id: 0 }]'
+     // Query filter to select all documents
+     // Projection: include name and age, exclude _id
+
 
 // Replace keys without quotes and then single quotes with double quotes
 let validJsonString = originalString
-    .replace(/([{,]\s*)(\w+)(\s*:)/g, '$1"$2"$3') // Wrap keys in double quotes
+    .replace(/([{,]\s*)([\$]*\w+)(\s*:)/g, '$1"$2"$3')// Wrap keys in double quotes
     .replace(/'/g, '"'); // Replace single quotes with double quotes
 
-
+console.log(validJsonString)
 function stringToObject(validJsonString)
 {
     try {
@@ -16,4 +19,4 @@ function stringToObject(validJsonString)
     }
 }
 let string = stringToObject(validJsonString)
-console.log(string[0])
+console.log(string)
