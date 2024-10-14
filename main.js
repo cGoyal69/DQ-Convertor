629b46f6ee6d761f250c3daa8b72d32f9b718a57
Chirag Goyal <cgoy96@gmail.com>
2024-10-14 22:56:10 +0530

    hello

--- main.js
+++ main.js
@@ -11,25 +11,27 @@ const createMongoToPGConverter = require('./jsonToPGSQL')
 const pgsqlToJSON = require('./pgsqlToJSON')
 
 
-const postgresQuery = `
-SELECT 
+const postgresQuery = `SELECT 
     c.customer_id,
     CONCAT(c.first_name, ' ', c.last_name) AS full_name,
     c.region,
-    c.total_spent,
-    c.total_orders,
-    c.total_products,
-    c.most_frequent_product,
-    c.region_rank,
-    c.global_rank,
-    AVG(c.total_spent) OVER () AS average_spending_global,
-    SUM(c.total_spent) OVER () AS total_spending_global,
-    COALESCE(c.last_order_date, '1970-01-01') AS last_order_date
-FROM customers c
-WHERE c.total_orders > 5 AND c.region IN ('North', 'South', 'East', 'West')
-GROUP BY c.customer_id, c.first_name, c.last_name, c.region, c.total_spent, c.total_orders, c.total_products, c.most_frequent_product, c.region_rank, c.global_rank, c.last_order_date
-HAVING c.total_spent > (SELECT AVG(total_spent) FROM customers)
-ORDER BY c.global_rank
-LIMIT 10 OFFSET 5;
+    SUM(o.total_amount) AS total_spent,
+    COUNT(o.order_id) AS total_orders,
+    AVG(o.total_amount) AS average_order_value,
+    MAX(o.order_date) AS last_order_date,
+    COALESCE(MAX(o.last_order_date), '1970-01-01') AS formatted_last_order_date
+FROM 
+    customers c
+LEFT JOIN 
+    orders o ON c.customer_id = o.customer_id
+WHERE 
+    c.region IN ('North', 'South', 'East', 'West')
+GROUP BY 
+    c.customer_id, c.first_name, c.last_name, c.region
+HAVING 
+    SUM(o.total_amount) > 1000
+ORDER BY 
+    total_spent DESC
+LIMIT 10;
 `;
 console.log(pgsqlToJSON(postgresQuery))
